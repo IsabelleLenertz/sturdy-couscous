5ef095d40fc4bedeee691c1f955cddde6fab80dd
janani-sridhar <janani.sridhar@yahoo.com>
2020-11-08 19:47:17 -0800

    added checking methods for port scanning, certificate checking, and cipher list

--- sturdycouscous/Garbanzo/Checker.py
+++ sturdycouscous/Garbanzo/Checker.py
@@ -4,6 +4,7 @@
 # 3) Look for open ports
 # 4) Get all ciphers supported (?)
 import socket, ssl
+from datetime import datetime
 
 class connection_checker():
 
@@ -16,14 +17,94 @@ class connection_checker():
 		return (result == 0)
 
 	# Checks whether the certificate obtained using the default_context is still valid.
+	# Returns True if certificate is still valid.
 	def certificate_checker(domain):
-		pass
+		context = ssl.create_default_context():
+		sslSocket = context.wrap_socket(s, server_hostname = domain)
+		sslSocket.connect((domain, 443))
+		
+		cert = ssl.getpeercert()
+		expiry_date = datetime.strptime(cert['notAfter'], "%b %d %H:%M:%S %Y %Z")
+
+		return datetime.now() < expiry_date
+
+
+	# Returns the list of all tls contexts
+	def get_all_tls_contexts():
+		# Contexts for each TLS version
+		tlsv1_3_context = ssl.create_default_context()
+		tlsv1_3_context.options |= (ssl.PROTOCOL_TLS | ssl.OP_NO_TLSv1 | ssl.OP_NO_TLSv1_1 | ssl.OP_NO_TLSv1_2)
+
+		tlsv1_2_context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+		tlsv1_1_context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_1)
+		tlsv1_context = ssl.SSLContext(ssl.PROTOCOL_TLSv1)
+
+		contexts_dict = {
+			tlsv1_context: "tlsv1",
+			tlsv1_1_context: "tlsv1_1",
+			tlsv1_2_context: "tlsv1_2",
+			tlsv1_3_context: "tlsv1_3",
+		}
+
+		contexts = [tlsv1_3_context, tlsv1_2_context, tlsv1_1_context, tlsv1_context]
+
+		return contexts_dict, contexts		
 
 	# Returns a list of all TLS verions supported for the given domain. 
 	def tls_versions_checker(domain):
-		pass
+		# Contexts for each TLS version
+		# tlsv1_3_context = ssl.create_default_context()
+		# tlsv1_3_context.options |= (ssl.PROTOCOL_TLS | ssl.OP_NO_TLSv1 | ssl.OP_NO_TLSv1_1 | ssl.OP_NO_TLSv1_2)
+
+		# tlsv1_2_context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+		# tlsv1_1_context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_1)
+		# tlsv1_context = ssl.SSLContext(ssl.PROTOCOL_TLSv1)
+
+		# contexts_dict = {
+		# 	tlsv1_context: "tlsv1",
+		# 	tlsv1_1_context: "tlsv1_1",
+		# 	tlsv1_2_context: "tlsv1_2",
+		# 	tlsv1_3_context: "tlsv1_3",
+		# }
+
+		# contexts = [tlsv1_3_context, tlsv1_2_context, tlsv1_1_context, tlsv1_context]
+		
+		contexts_dict, contexts = get_all_tls_contexts()
+		versions_supported = []
+
+		for context in contexts:
+			if test_tls_version(context, domain):
+				versions_supported.append(contexts_dict[context])
+
+		return {domain: versions_supported}
+
+
+	def test_tls_version(context, domain):
+		success = True
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)	
+			sslSocket = context.wrap_socket(s, server_hostname = domain)
+			sslSocket.connect((domain, 443))
+			print(sslSocket.version())
+		except:
+			success = False
+		finally:
+			return success
+	
 
 	# Returns a list of all ciphers supported by... context...? 
 	# Need more clarification on this one.
 	def get_all_ciphers(domain):
-		pass
+
+		context = ssl.create_default_context()
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)	
+		sslSocket = context.wrap_socket(s, server_hostname = domain)
+
+		sslSocket.connect((domain, 443))
+		all_ciphers = sslSocket.shared_ciphers()
+
+		return all_ciphers
+
+
+
+
