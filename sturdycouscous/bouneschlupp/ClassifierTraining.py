bedadc43f143ee3b9ea7b2d977580b0d10799004
Isabelle Delmas <isabelle.lenertz@gmail.com>
2020-11-29 06:10:14 +0000

    working on the trainer

--- sturdycouscous/bouneschlupp/ClassifierTraining.py
+++ sturdycouscous/bouneschlupp/ClassifierTraining.py
@@ -11,15 +11,24 @@
 import csv
 import requests
 import errors
-
+from bs4 import BeautifulSoup
 
 
 with open("sturdycouscous/resources/keyword_training.csv") as csvfile:
     training_sample = csv.reader(csvfile, delimiter=',')
     data_table={}
+    response = requests.get('https://stackoverflow.com/')
+    if response.status_code == 200:
+        content = BeautifulSoup(response.content, 'html.parser')
+        metadata = content.head.find_all("meta")
+        for tag in metadata:
+            print(tag)
+    else:
+        print(response.status_code)
+    '''
     for row in training_sample:
         try:
-            response = requests.get(row[0])
+            response = requests.get('https://stackoverflow.com/')
             if response.status_code == 200:
                 content = BeautifulSoup(response.content, 'html.parser')
                 metadata = content.head.find_all("metadata")
@@ -29,6 +38,7 @@ with open("sturdycouscous/resources/keyword_training.csv") as csvfile:
                         print(kw_list)
             else:
                 raise errors.BadResponseError(response.status_code)
-        except BaseException :
-            print("failled get for", row[0])
-            pass
+        except BaseException as e:
+            print(type(e))
+    '''
+    
