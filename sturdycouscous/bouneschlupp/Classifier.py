a98ee68dcb555faf9e79a723673c510415c9e3e9
Isabelle Delmas <isabelle.lenertz@gmail.com>
2020-12-07 05:57:20 +0000

    Classifier training working

--- sturdycouscous/bouneschlupp/Classifier.py
+++ sturdycouscous/bouneschlupp/Classifier.py
@@ -1,12 +1,41 @@
 from enum import Enum
 from bs4 import BeautifulSoup
-from ClassifierTraining import Category
-import mongoengine as me
+from pymongo import MongoClient, errors
+from nltk.tokenize import word_tokenize
+from nltk.stem.porter import PorterStemmer
+from nltk.corpus import stopwords
 
-""" Constents used  by the Classifier module"""
-class Category(me.Document):
-    id = me.StringField(required=True, unique = True, primary_key = True)
-    keywords = me.ListField(field = me.StringField(), required=True)
+DOMAIN = 'couscousmongo'
+PORT = 27017
+DB_NAME = "couscous_db"
+COLLECTION = "Categories"
+CATEGORIES = ['IT', 'government', 'education', 'news', 'other', 'commerce', 'social-media']
+
+def connect_client():
+    try:
+        client = MongoClient(
+                host = [ str(DOMAIN) + ":" + str(PORT) ],
+                serverSelectionTimeoutMS = 3000, # 3 second timeout
+                username = "root",
+                password = "root"
+        )
+        return client
+    except errors.ServerSelectionTimeoutError as err:
+        print("pymongo ERROR: ", err)
+        return None
+
+def clean_tokens(text):
+    tokens = word_tokenize(text)
+    tokens = [w.lower() for w in tokens]
+    table = str.maketrans('', '', string.punctuation)
+    stripped = [w.translate(table) for w in tokens]
+    words = [word for word in stripped if word.isalpha()]
+    stop_words = set(stopwords.words('english'))
+    words = [w for w in words if not w in stop_words]
+    # Stemming
+    porter = PorterStemmer()
+    return [porter.stem(word) for word in words]
+
 class Classifier:
     DOMAIN = 'couscousmongo'
     PORT = 27017
@@ -23,17 +52,15 @@ class Classifier:
         keywords = None
         evaluation = {}
         # Count keywords in each category
-        me.connect(username=self.USERNAME, 
-           password=self.PASSWORD, 
-           port=self.PORT, 
-           host=self.DOMAIN, 
-           authentication_source='admin',
-           db=self.DB_NAME)
-
-        for category in Category.objects:
-            counter = 0;
+        client = connect_client()
+        db = client[DB_NAME]
+        collection = db[COLLECTION]
+        categories = collection.find()
+        for category in CATEGORIES:
+            category_keywords = collection.find({'_id': category}).get('keywords')
+            counter = 0
             for word in keywords:
-                if word in category.keywords:
+                if word in category_keywords:
                     counter += 1 
             evaluation.insert(category.id, counter)
         
